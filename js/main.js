6a93bdff1b6f95d79205b7de3389d42d5a35850a
276466206@qq.com <wuqiong@DuoLunTechnology.com>
2017-04-05 17:01:12 +0800

    some

--- js/main.js
+++ js/main.js
@@ -1,13 +1,24 @@
 var $hour=$('.hour'), $minute=$('.minute'), $second=$('.second');
-
 setInterval(function(){
 	var date=new Date();
 	var hour=date.getHours(),
 	minute=date.getMinutes(),
 	second=date.getSeconds();
-	var nowTime=(24-hour)*60*60+(00-minute)*60+(00-second);
-		$hour.text(parseInt(nowTime/(60*60)));
-		$minute.text(parseInt(nowTime/60%60));
-	$second.text(parseInt(nowTime%60));
+	var nowTime=(24-hour)*60*60-minute*60-second;
+	function toTime(time){
+		if(time<10){
+			time='0'+time;
+			
+		}else {
+			time=time;
+		}
+		return time;
+	}
+	var nowHour=parseInt(toTime(nowTime/(60*60))),
+	nowMinute=parseInt(toTime(nowTime/60%60)),
+	nowSecond=parseInt(toTime(nowTime%60));
+	$hour.text(toTime(nowHour));
+	$minute.text(toTime(nowMinute));
+	$second.text(toTime(nowSecond));
 	},1000)
 	
